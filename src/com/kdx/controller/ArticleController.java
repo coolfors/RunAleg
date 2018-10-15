30434e66c5bc1fde409021aaa00cf49296cea47d
Administrator <Administrator@stu1003>
2018-10-15 15:39:40 +0800

    测试阿里编码规范

--- src/com/kdx/controller/ArticleController.java
+++ src/com/kdx/controller/ArticleController.java
@@ -1,26 +1,29 @@
 package com.kdx.controller;
 
+
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.google.gson.Gson;
 import com.kdx.entity.Article;
-import com.kdx.entity.Dispatch;
-import com.kdx.entity.Evaluate;
 import com.kdx.service.ArticleService;
 import com.kdx.serviceImpl.ArticleServiceImpl;
-import com.kdx.util.PageData;
-
 /**
  * Servlet implementation class QusController
  */
 @WebServlet("/ArtcleController")
+/**
+ * 
+ * @author coolfors
+ *
+ */
 public class ArticleController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -28,6 +31,7 @@ public class ArticleController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		/*更新文章提交到文章页面*/
@@ -35,31 +39,30 @@ public class ArticleController extends HttpServlet {
 		request.setCharacterEncoding("utf-8");
 		//输出控制台
 		PrintWriter out = response.getWriter();
-		//获取传送的参数值
+		//获取传送的参数值系统时间
 		String article = request.getParameter("article");
-		String articledate = new Date().toLocaleString();
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String articledate = df.format( new Date());
 		String title = request.getParameter("title");
-		
-		System.out.println(article);
-		System.out.println("作者：" + request.getParameter("username"));
+		//添加数据到数据库
 		Article at = new Article(8,articledate, title, article);
 		ArticleService as = new ArticleServiceImpl();
 		boolean flag  = as.addArticle(at);
 		if(flag) {
-		System.out.println("添加文章成功");
+			//将数据转成json串
 		Gson gson = new Gson();
 		String jsonstr = gson.toJson(at);
-		System.out.println("jsonstr"+jsonstr);
-		out.println(jsonstr);
-		out.close();
+		//添加成功转发到所有文章界面
 		request.setAttribute("pd", jsonstr);
 		request.getRequestDispatcher("admin/Aarticle.html").forward(request, response);
 	}
 	}
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
