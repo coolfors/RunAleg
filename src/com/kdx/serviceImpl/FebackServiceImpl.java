3bcd688e1138c72c13f60902d1765b7302a1d23c
Administrator <Administrator@PC201807121647>
2018-10-17 09:31:38 +0800

    chenhan

---
+++ src/com/kdx/serviceImpl/FebackServiceImpl.java
@@ -0,0 +1,35 @@
+package com.kdx.serviceImpl;
+
+import com.kdx.dao.FebackDao;
+import com.kdx.daoImpl.FebackDaoImpl;
+import com.kdx.entity.Feedback;
+import com.kdx.service.FebackService;
+import com.kdx.util.PageData;
+
+public class FebackServiceImpl implements FebackService {
+	private FebackDao fd=new FebackDaoImpl();
+	@Override
+	public boolean addFeback(Feedback f) {
+		// TODO Auto-generated method stub
+		return fd.addFeback(f);
+	}
+
+	@Override
+	public boolean delFeback(int feedbackId) {
+		// TODO Auto-generated method stub
+		return fd.delFeback(feedbackId);
+	}
+
+	@Override
+	public boolean updateFeback(Feedback f) {
+		// TODO Auto-generated method stub
+		return fd.updateFeback(f);
+	}
+
+	@Override
+	public PageData<Feedback> queryFebackByPage(int page, int pageSize,int userId) {
+		// TODO Auto-generated method stub
+		return fd.queryFebackByPage(page, pageSize,userId);
+	}
+
+}
